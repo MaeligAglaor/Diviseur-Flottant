5f3ba603cf41c307157fa3922beb79a3bf7dc219
MaeligAglaor <takolepoulpe@gmail.com>
2015-01-26 14:04:46 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+float f,f1;
+
+cout<< "Veuillez entrez le premier nombre"<<endl;
+cin>> f;
+cout<< "Veuillez entrez le deuxième nombre"<<endl;
+cin>> f1;
+
+float f2 = f/f1;
+
+cout<< " La division de "<<f<<" par "<<f1<<" est égale à "<<f2;
+
+return 0;
+
+
+}
